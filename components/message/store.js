e9ddc4c996f8469016a9c16c8859417589d91195
Ruben Hernandez <ruben.hernandez.puerta@rubdevs.com>
2020-10-16 18:56:09 -0500

    Mongoose configured to work with Mongo dB Atlas cluster

--- components/message/store.js
+++ components/message/store.js
@@ -1,10 +1,20 @@
-const list = [];
+const db = require('mongoose');
+const Model = require('./model');
 
+db.Promise = global.Promise;
+db.connect('mongodb+srv://db_user_chat_node:GEtNpxWiWklkMYo3@cluster0.dljzv.gcp.mongodb.net/chat_node?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+console.log('[db] Conectada con exito')
 function addMessage(message){
-    list.push(message);
+    const myMessage = new Model(message);
+    myMessage.save();
 }
 
-function getMessages(){
+async function getMessages(){
+    const list = await Model.find();
     return list;
 }
 
