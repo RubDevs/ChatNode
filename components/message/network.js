ed232ba702425adb88b00bb549c8e45e84bd405c
Ruben Hernandez <ruben.hernandez.puerta@rubdevs.com>
2020-10-17 11:55:18 -0500

    Multer added

--- components/message/network.js
+++ components/message/network.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const multer =  require('multer');
 const controller = require('./controller')
 const response = require('../../network/response');
 
